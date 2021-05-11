9bb07c0d99490ba711bf1b304f89eb18d288cd6b
MariyaZlateva <mimitoooo@abv.bg>
2021-05-11 15:07:25 +0300

    Connecting MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin:Sweetgirl@cluster0.ehesn.mongodb.net/moviemaker?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+    .then(()=> console.log('MongoDB Connected!'))
+    .catch((err)=> console.log('Error!', err))
 
 app.get('/', (req, res) => {
     res.send('Hello from backend app.js')
 })
 
-app.listen(4000, ()=>{
+app.listen(4000, () => {
     console.log('Server on port 4000')
 })
 
