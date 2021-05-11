d6f8264d7cc13ba30cd30f880117c8fee3782f72
MariyaZlateva <mimitoooo@abv.bg>
2021-05-11 15:33:30 +0300

    Setting up GraphQL

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const {graphqlHTTP} = require('express-graphql');
+const {buildSchema} = require('graphql');
 
 mongoose.connect('mongodb+srv://admin:Sweetgirl@cluster0.ehesn.mongodb.net/moviemaker?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -9,6 +11,25 @@ mongoose.connect('mongodb+srv://admin:Sweetgirl@cluster0.ehesn.mongodb.net/movie
 })
     .then(()=> console.log('MongoDB Connected!'))
     .catch((err)=> console.log('Error!', err))
+
+const schema = buildSchema(`
+type Query {
+    name: String
+}
+`)
+
+const rootValue = {
+    name: ()=> {
+        return 'John Wick Parebellum'
+    }
+}
+
+//Setting GraphQL
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+    rootValue
+}))
 
 app.get('/', (req, res) => {
     res.send('Hello from backend app.js')
